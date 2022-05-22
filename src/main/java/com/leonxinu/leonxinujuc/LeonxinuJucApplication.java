0b100737857fee010bba6d7ba9a3259bf735f949
leonxinu <2550784872@qq.com>
2022-05-22 17:13:34 +0800

    juc study

---
+++ src/main/java/com/leonxinu/leonxinujuc/LeonxinuJucApplication.java
@@ -0,0 +1,16 @@
+package com.leonxinu.leonxinujuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 25507
+ */
+@SpringBootApplication
+public class LeonxinuJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeonxinuJucApplication.class, args);
+    }
+
+}
